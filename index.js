b2790b15be79c5800488382e74e39c5b13b4fdbe
xkomandosx <pawel.pastucha@gmail.com>
2023-08-28 13:15:01 +0200

    downgrading

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-'use strict';
-var through = require('through2');
-var encode = require('./lib/encode');
+"use strict";
+var through = require("through2");
+var encode = require("./lib/encode");
 
 module.exports = function (opts) {
+  function rebase(file, encoding, callback) {
+    var self = this;
 
-    function rebase(file, encoding, callback) {
-        var self = this;
+    encode.stylesheet(file, opts, function (err, src) {
+      if (err) {
+        console.error(err);
+      }
+      file.contents = new Buffer(src);
 
-        encode.stylesheet(file, opts, function (err, src) {
-            if (err) {
-                console.error(err);
-            }
-            file.contents = new Buffer(src);
+      self.push(file);
+      callback();
+    });
+  }
 
-            self.push(file);
-            callback();
-        });
-
-    }
-
-    return through.obj(rebase);
+  return through.obj(rebase);
 };
